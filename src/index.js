052ab86e0f8db880ee1feab9ebcea807762a56f3
michealCSMB <mwhaley@csmb-stl.org>
2020-07-29 17:10:33 -0500

    navbar

--- src/index.js
+++ src/index.js
@@ -2,10 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
 import App from './App';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#flights">Flights</Nav.Link>
+      <Nav.Link href="#booking">Booking</Nav.Link>
+      <Nav.Link href="#ticket">Ticket</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+<App />
   </React.StrictMode>,
   document.getElementById('root')
 );
