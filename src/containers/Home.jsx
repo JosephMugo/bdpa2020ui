4f57a57e9068f1d17d4bccaab96e2e08a9898ac4
michealCSMB <mwhaley@csmb-stl.org>
2020-08-01 21:04:46 -0500

    made the home a bit more nice

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,9 +1,47 @@
 import React from "react"
+import Carousel from "react-bootstrap/Carousel"
 const Home = () => {
 
     return (
         <div>
-            These are home
+            <h1 style={{ textAlign:"center"}} >Welcome</h1>
+            <Carousel style={{width: "75%", margin: "auto"}}>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="https://cdn-image.travelandleisure.com/sites/default/files/styles/1600x1000/public/1459190417/Boeing-TEST0316.jpg?itok=hExHhy9s"
+                    alt="First slide"
+                    />
+                    <Carousel.Caption>
+                    <h3>Planes</h3>
+                    <p>For all your booking needs</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="https://wallpapercave.com/wp/9aungfd.jpg"
+                    alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                    <h3>Another Plane</h3>
+                    <p>Plane knowledge</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="https://wonderfulengineering.com/wp-content/uploads/2014/05/airplane-wallpaper-3.jpg"
+                    alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                    <h3>Planes</h3>
+                    <p>Description</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </div>
     )
 }
