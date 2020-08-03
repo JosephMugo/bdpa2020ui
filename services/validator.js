99f43e16140f0d6ec9c7df61e65fb695b92d9e64
“spicecat” <ateh0701@gmail.com>
2020-08-03 13:27:31 -0500

    Added form template to Login and SignUp

---
+++ services/validator.js
@@ -0,0 +1,5 @@
+import isEmail from "validator/lib/isEmail"
+const verifySignUp = (user) => {
+
+}
+module.export(verifySignUp)
--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,10 +1,28 @@
-import React from "react"
+import React, { useState } from "react"
+import { Form, Button, Col } from 'react-bootstrap'
 const SignIn = () => {
-
+    // const [first_name, setFirstName] = useState(""), [last_name, setLastName] = useState(""), [password, setPassword] = useState(""), [isValid, setValidity] = useState("")
     return (
-        <div>
-            Login
-        </div>
+        <Form>
+            <Form.Row>
+                <Form.Group as={Col} controlId="first_name">
+                    <Form.Label>First name</Form.Label>
+                    <Form.Control type="text" placeholder="First name" />
+                </Form.Group>
+                <Form.Group as={Col} controlId="last_name">
+                    <Form.Label>Last name</Form.Label>
+                    <Form.Control type="text" placeholder="Last name" />
+                </Form.Group>
+            </Form.Row>
+            <Form.Group controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group controlId="remember">
+                <Form.Check type="checkbox" label="Remember" />
+            </Form.Group>
+            <Button variant="primary" type="submit">Login</Button>
+        </Form>
     )
 }
 
