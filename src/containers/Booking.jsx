a3092ba4d15da883690d683fef148fcbd604dec3
michealCSMB <mwhaley@csmb-stl.org>
2020-08-03 15:58:15 -0500

    home a bit better and basic booking stuff

--- src/containers/Booking.jsx
+++ src/containers/Booking.jsx
@@ -1,23 +1,124 @@
 import React from "react"
-const Bookings = () => {
+import InputGroup from 'react-bootstrap/InputGroup'
+import { FormControl } from "react-bootstrap"
 
-    return (
-        <div>
-            Where do you want to arrive at?
-            Answer:place and time
-            If user hasn't been to flight view yet it will show a view of flights that meet requirment
-            Name
-            Sex
-            Birthday
-            Card info and what not
+const Bookings = (flights) => {
+    if (flights === true) { //if there coming from the flights section it'll skip asking them where and when they wanna go
+        return (
+            <div>this is an empty div</div>
+        )
+    } else {
+        return (
+            <div>
+                <InputGroup className="mb-3" style={{ width: "300px" }}>
+                <InputGroup.Prepend>
+                        <InputGroup.Text id="basic-addon1">Location:</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                        placeholder="City"
+                        aria-label="Month"
+                        aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
+
+                <InputGroup className="mb-3" style={{ width: "250px" }}>
+                    <FormControl
+                        placeholder="MM"
+                        aria-label="Month"
+                        aria-describedby="basic-addon1"
+                    />
+                    <InputGroup.Prepend>
+                        <InputGroup.Text id="basic-addon1">/</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                        placeholder="DD"
+                        aria-label="Day"
+                        aria-describedby="basic-addon1"
+                    />
+                    <InputGroup.Prepend>
+                        <InputGroup.Text id="basic-addon1">/</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                        placeholder="YY"
+                        aria-label="Year"
+                        aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
+
+                <InputGroup className="mb-3" style={{ width: "300px" }}>
+                <InputGroup.Prepend>
+                        <InputGroup.Text id="basic-addon1">First Name:</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                        aria-label="FirstName"
+                        aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
+                <InputGroup className="mb-3" style={{ width: "300px" }}>
+                <InputGroup.Prepend>
+                        <InputGroup.Text id="basic-addon1">Middle Name:</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                        placeholder="Optional"
+                        aria-label="MiddleName"
+                        aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
+                <InputGroup className="mb-3" style={{ width: "300px" }}>
+                <InputGroup.Prepend>
+                        <InputGroup.Text id="basic-addon1">Last Name:</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                        aria-label="LastName"
+                        aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
+                <InputGroup className="mb-3" style={{ width: "300px" }}>
+                <InputGroup.Prepend>
+                        <InputGroup.Text id="basic-addon1">Sex:</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                        aria-label="Sex"
+                        aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
+
+                <InputGroup className="mb-3" style={{ width: "350px" }}>
+                <InputGroup.Prepend>
+                        <InputGroup.Text id="basic-addon1">Brithday:</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                        placeholder="MM"
+                        aria-label="Month"
+                        aria-describedby="basic-addon1"
+                    />
+                    <InputGroup.Prepend>
+                        <InputGroup.Text id="basic-addon1">/</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                        placeholder="DD"
+                        aria-label="Day"
+                        aria-describedby="basic-addon1"
+                    />
+                    <InputGroup.Prepend>
+                        <InputGroup.Text id="basic-addon1">/</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                        placeholder="YY"
+                        aria-label="Year"
+                        aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
+            Card info and what not: exp date, zip code, type of card
             If logged in ask to save info to not type it later
             Select seat number
             How many bags
             first is free second carry on is $30 second checked is $50 and more is $100
             max 2 carry-on and 5 checked
             must book 24 hours before flight
-        </div>
-    )
+            </div>
+        )
+    }
 }
 
 export default Bookings
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,13 +1,14 @@
 import React from "react"
 import Carousel from "react-bootstrap/Carousel"
+import Image from 'react-bootstrap/Image'
 const Home = () => {
 
     return (
         <div>
             <h1 style={{ textAlign:"center"}} >Welcome</h1>
-            <Carousel style={{width: "75%", margin: "auto"}}>
+            <Carousel style={{width: "75%", margin: "auto",borderRadius:"55px 55px 55px 55px",overflow: "hidden"}}>
                 <Carousel.Item>
-                    <img
+                    <Image rounded
                     className="d-block w-100"
                     src="https://cdn-image.travelandleisure.com/sites/default/files/styles/1600x1000/public/1459190417/Boeing-TEST0316.jpg?itok=hExHhy9s"
                     alt="First slide"
@@ -18,7 +19,7 @@ const Home = () => {
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img
+                    <Image
                     className="d-block w-100"
                     src="https://wallpapercave.com/wp/9aungfd.jpg"
                     alt="Third slide"
@@ -30,7 +31,7 @@ const Home = () => {
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img
+                    <Image
                     className="d-block w-100"
                     src="https://wonderfulengineering.com/wp-content/uploads/2014/05/airplane-wallpaper-3.jpg"
                     alt="Third slide"
