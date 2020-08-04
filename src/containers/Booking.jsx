be3d8182d6b8b1f8a2d1673edec979bcae87502e
JosephMugo <joemu18@gmail.com>
2020-08-04 12:09:53 -0500

    changed Bookings.js & Login.js to have required in the input fields

--- src/containers/Booking.jsx
+++ src/containers/Booking.jsx
@@ -1,8 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
 import InputGroup from 'react-bootstrap/InputGroup'
 import { FormControl } from "react-bootstrap"
+import { Form, Button, Col } from 'react-bootstrap'
 
 const Bookings = (flights) => {
+    const [isValid, setValid] = useState("")
+    const formGroup = (label, size, isRequired) => {
+        return (
+            <Form.Group as={Col} md={size} controlId={label.toLowerCase().replace(" ", "_")}>
+                <Form.Label>{label}</Form.Label>
+                {isRequired ? <Form.Control required type="text" placeholder={label} /> : <Form.Control type="text" placeholder={label} />}
+                {isRequired && <Form.Control.Feedback type="invalid">Required</Form.Control.Feedback>}
+            </Form.Group>
+        )
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        event.stopPropagation()
+        setValid(true)
+    }
     if (flights === true) { //if there coming from the flights section it'll skip asking them where and when they wanna go
         return (
             <div>this is an empty div</div>
@@ -10,112 +27,26 @@ const Bookings = (flights) => {
     } else {
         return (
             <div>
-                <InputGroup className="mb-3" style={{ width: "300px" }}>
-                <InputGroup.Prepend>
-                        <InputGroup.Text id="basic-addon1">Location:</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl
-                        placeholder="City"
-                        aria-label="Month"
-                        aria-describedby="basic-addon1"
-                    />
-                </InputGroup>
-
-                <InputGroup className="mb-3" style={{ width: "250px" }}>
-                    <FormControl
-                        placeholder="MM"
-                        aria-label="Month"
-                        aria-describedby="basic-addon1"
-                    />
-                    <InputGroup.Prepend>
-                        <InputGroup.Text id="basic-addon1">/</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl
-                        placeholder="DD"
-                        aria-label="Day"
-                        aria-describedby="basic-addon1"
-                    />
-                    <InputGroup.Prepend>
-                        <InputGroup.Text id="basic-addon1">/</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl
-                        placeholder="YY"
-                        aria-label="Year"
-                        aria-describedby="basic-addon1"
-                    />
-                </InputGroup>
-
-                <InputGroup className="mb-3" style={{ width: "300px" }}>
-                <InputGroup.Prepend>
-                        <InputGroup.Text id="basic-addon1">First Name:</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl
-                        aria-label="FirstName"
-                        aria-describedby="basic-addon1"
-                    />
-                </InputGroup>
-                <InputGroup className="mb-3" style={{ width: "300px" }}>
-                <InputGroup.Prepend>
-                        <InputGroup.Text id="basic-addon1">Middle Name:</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl
-                        placeholder="Optional"
-                        aria-label="MiddleName"
-                        aria-describedby="basic-addon1"
-                    />
-                </InputGroup>
-                <InputGroup className="mb-3" style={{ width: "300px" }}>
-                <InputGroup.Prepend>
-                        <InputGroup.Text id="basic-addon1">Last Name:</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl
-                        aria-label="LastName"
-                        aria-describedby="basic-addon1"
-                    />
-                </InputGroup>
-                <InputGroup className="mb-3" style={{ width: "300px" }}>
-                <InputGroup.Prepend>
-                        <InputGroup.Text id="basic-addon1">Sex:</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl
-                        aria-label="Sex"
-                        aria-describedby="basic-addon1"
-                    />
-                </InputGroup>
-
-                <InputGroup className="mb-3" style={{ width: "350px" }}>
-                <InputGroup.Prepend>
-                        <InputGroup.Text id="basic-addon1">Brithday:</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl
-                        placeholder="MM"
-                        aria-label="Month"
-                        aria-describedby="basic-addon1"
-                    />
-                    <InputGroup.Prepend>
-                        <InputGroup.Text id="basic-addon1">/</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl
-                        placeholder="DD"
-                        aria-label="Day"
-                        aria-describedby="basic-addon1"
-                    />
-                    <InputGroup.Prepend>
-                        <InputGroup.Text id="basic-addon1">/</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl
-                        placeholder="YY"
-                        aria-label="Year"
-                        aria-describedby="basic-addon1"
-                    />
-                </InputGroup>
-            Card info and what not: exp date, zip code, type of card
-            If logged in ask to save info to not type it later
-            Select seat number
-            How many bags
-            first is free second carry on is $30 second checked is $50 and more is $100
-            max 2 carry-on and 5 checked
-            must book 24 hours before flight
+                <h2>Bookings</h2>
+                <Form noValidate validated={isValid} onSubmit={handleSubmit}>
+                    <Form.Row>
+                        {formGroup("First Name", 6, true)}
+                        {formGroup("Last Name", 6, true)}
+                    </Form.Row>
+                    <Form.Group controlId="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control required type="password" aria-describedby="passwordHelpBlock" />
+                        <Form.Control.Feedback type="invalid">Required</Form.Control.Feedback>
+                    </Form.Group>
+                    <Button type="submit" >Register</Button>
+                </Form>
+                Card info and what not: exp date, zip code, type of card
+                If logged in ask to save info to not type it later
+                Select seat number
+                How many bags
+                first is free second carry on is $30 second checked is $50 and more is $100
+                max 2 carry-on and 5 checked
+                must book 24 hours before flight
             </div>
         )
     }
