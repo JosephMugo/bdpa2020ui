bd6485046f2d97f4bc659b2c06e9daec3948e567
unknown <jasonz4200@gmail.com>
2020-08-04 15:31:11 -0500

    Fix merge conflicts

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from "react"
-import { Form, Button, Col } from 'react-bootstrap'
-
-
-const SignIn = () => {
-    const [first_name, setFirstName] = useState(""), [last_name, setLastName] = useState(""), [password, setPassword] = useState(""), [isValid, setValid] = useState("")
-    const formGroup = (label, size, isRequired) => {
-        return (
-            <Form.Group as={Col} md={size} controlId={label.toLowerCase().replace(" ", "_")}>
-                <Form.Label>{label}</Form.Label>
-                {isRequired ? <Form.Control required type="text" placeholder={label} /> : <Form.Control type="text" placeholder={label} />}
-                {isRequired && <Form.Control.Feedback type="invalid">Required</Form.Control.Feedback>}
-            </Form.Group>
-        )
-    }
-
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        event.stopPropagation()
-        setValid(true)
-    }
-
-    return (
-        <>
-            <div className="row">
-                <div className='col-sm-4'>
-                    <h2>Login</h2>
-                    <Form noValidate validated={isValid} onSubmit={handleSubmit}>
-                        <Form.Row>
-                            {formGroup("First Name", 6, true)}
-                            {formGroup("Last Name", 6, true)}
-                        </Form.Row>
-                        <Form.Group controlId="password">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control required type="password" aria-describedby="passwordHelpBlock" />
-                            <Form.Control.Feedback type="invalid">Required</Form.Control.Feedback>
-                        </Form.Group>
-                        <Button type="submit" >Register</Button>
-                    </Form>
-                </div>
-            </div>
-=======
 import React from "react"
 import { Formik, Field, Form, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
@@ -48,48 +5,51 @@ import { login } from "../services/userService";
 const Login = () => {
     return (
         <>
-            <h2>Login</h2>
-            <Formik
-                initialValues={{
-                    firstName: "", lastName: "", password: ""
-                }}
-                validationSchema={Yup.object().shape({
-                    firstName: Yup.string().required('Required'),
-                    lastName: Yup.string().required('Required'),
-                    password: Yup.string().required('Required')
-                })}
-                onSubmit={fields => {
-                    console.log(fields)
-                    login(fields.firstName + fields.lastName, fields.password)
-                }}
-            >
-                {({ errors, status, touched }) => (
-                    <Form>
-                        <div className="form-row">
-                            <div className="form-group col">
-                                <label htmlFor="firstName">First Name</label>
-                                <Field name="firstName" type="text" className={'form-control' + (errors.firstName && touched.firstName ? ' is-invalid' : '')} />
-                                <ErrorMessage name="firstName" component="div" className="invalid-feedback" />
-                            </div>
-                            <div className="form-group col">
-                                <label htmlFor="lastName">Last Name</label>
-                                <Field name="lastName" type="text" className={'form-control' + (errors.lastName && touched.lastName ? ' is-invalid' : '')} />
-                                <ErrorMessage name="lastName" component="div" className="invalid-feedback" />
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="password">Password</label>
-                            <Field name="password" type="password" className={'form-control' + (errors.password && touched.password ? ' is-invalid' : '')} />
-                            <ErrorMessage name="password" component="div" className="invalid-feedback" />
-                        </div>
-                        <div className="form-group">
-                            <button type="submit" className="btn btn-primary mr-2">Register</button>
-                            <button type="reset" className="btn btn-secondary">Reset</button>
-                        </div>
-                    </Form>
-                )}
-            </Formik>
->>>>>>> ec1a2cd1b7cc2393246469327a620247c6d68b7b
+            <div className='row'>
+                <div className='col-sm-4'>
+                    <h2>Login</h2>
+                    <Formik
+                        initialValues={{
+                            firstName: "", lastName: "", password: ""
+                        }}
+                        validationSchema={Yup.object().shape({
+                            firstName: Yup.string().required('Required'),
+                            lastName: Yup.string().required('Required'),
+                            password: Yup.string().required('Required')
+                        })}
+                        onSubmit={fields => {
+                            console.log(fields)
+                            login(fields.firstName + fields.lastName, fields.password)
+                        }}
+                    >
+                        {({ errors, status, touched }) => (
+                            <Form>
+                                <div className="form-row">
+                                    <div className="form-group col">
+                                        <label htmlFor="firstName">First Name</label>
+                                        <Field name="firstName" type="text" className={'form-control' + (errors.firstName && touched.firstName ? ' is-invalid' : '')} />
+                                        <ErrorMessage name="firstName" component="div" className="invalid-feedback" />
+                                    </div>
+                                    <div className="form-group col">
+                                        <label htmlFor="lastName">Last Name</label>
+                                        <Field name="lastName" type="text" className={'form-control' + (errors.lastName && touched.lastName ? ' is-invalid' : '')} />
+                                        <ErrorMessage name="lastName" component="div" className="invalid-feedback" />
+                                    </div>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="password">Password</label>
+                                    <Field name="password" type="password" className={'form-control' + (errors.password && touched.password ? ' is-invalid' : '')} />
+                                    <ErrorMessage name="password" component="div" className="invalid-feedback" />
+                                </div>
+                                <div className="form-group">
+                                    <button type="submit" className="btn btn-primary mr-2">Register</button>
+                                    <button type="reset" className="btn btn-secondary">Reset</button>
+                                </div>
+                            </Form>
+                        )}
+                    </Formik>
+                </div>
+            </div>
         </>
     )
 }
--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -11,8 +11,7 @@ const securityQuestions = [
 const Register = () => {
     return (
         <>
-<<<<<<< HEAD:src/containers/SignUp.jsx
-            <div className="row">
+            <div className='row'>
                 <div className='col-sm-4'>
                     <h2>Register New Account</h2>
                     <Formik
@@ -46,8 +45,8 @@ const Register = () => {
                             captcha: Yup.string().matches("78", 'Invalid Captcha').required('Required')
                         })}
                         onSubmit={fields => {
-                            alert('SUCCESS!! :-)\n\n' + JSON.stringify(fields, null, 4))
-                            // createUser()
+                            const { captcha, ...info } = fields
+                            createUser(info)
                         }}
                     >
                         {({ errors, status, touched }) => (
@@ -160,152 +159,6 @@ const Register = () => {
                     </Formik>
                 </div>
             </div>
-=======
-            <h2>Register New Account</h2>
-            <Formik
-                initialValues={{
-                    title: "", firstName: "", middleName: "", lastName: "", suffix: "",
-                    birthdate: "", sex: "", city: "", state: "", zip: "", country: "",
-                    email: "", phone: "", password: "", securityQuestion1: "", securityQuestion2: "", securityQuestion3: "", captcha: ""
-                }}
-                validationSchema={Yup.object().shape({
-                    firstName: Yup.string().required('Required'),
-                    lastName: Yup.string().required('Required'),
-                    birthdate: Yup.string().required('Required'),
-                    city: Yup.string().required('Required'),
-                    state: Yup.string().required('Required'),
-                    zip: Yup.string().required('Required'),
-                    country: Yup.string().required('Required'),
-                    email: Yup.string()
-                        .email('Email is invalid')
-                        .required('Required'),
-                    password: Yup.string()
-                        .min(10, 'Must be at least 10 characters')
-                        .max(30, "Must be less than 30 characters")
-                        .required('Required')
-                        .matches(
-                            /^[a-zA-Z0-9!@#$%^&*?_~]+$/,
-                            "Cannot contain special characters or spaces"
-                        ),
-                    securityQuestion1: Yup.string().required('Required'),
-                    securityQuestion2: Yup.string().required('Required'),
-                    securityQuestion3: Yup.string().required('Required'),
-                    captcha: Yup.string().matches("78", 'Invalid Captcha').required('Required')
-                })}
-                onSubmit={fields => {
-                    const { captcha, ...info } = fields
-                    createUser(info)
-                }}
-            >
-                {({ errors, status, touched }) => (
-                    <Form>
-                        <div className="form-row">
-                            <div className="form-group col-2">
-                                <label htmlFor="title">Title</label>
-                                <Field name="title" type="text" className="form-control" />
-                            </div>
-                            <div className="form-group col-3">
-                                <label htmlFor="firstName">First Name</label>
-                                <Field name="firstName" type="text" className={'form-control' + (errors.firstName && touched.firstName ? ' is-invalid' : '')} />
-                                <ErrorMessage name="firstName" component="div" className="invalid-feedback" />
-                            </div>
-                            <div className="form-group col-3">
-                                <label htmlFor="middleName">Middle Name</label>
-                                <Field name="middleName" type="text" className="form-control" />
-                            </div>
-                            <div className="form-group col-3">
-                                <label htmlFor="lastName">Last Name</label>
-                                <Field name="lastName" type="text" className={'form-control' + (errors.lastName && touched.lastName ? ' is-invalid' : '')} />
-                                <ErrorMessage name="lastName" component="div" className="invalid-feedback" />
-                            </div>
-                            <div className="form-group col-1">
-                                <label htmlFor="suffix">Suffix</label>
-                                <Field name="suffix" type="text" className="form-control" />
-                            </div>
-                        </div>
-                        <div className="form-row">
-                            <div className="form-group col">
-                                <label htmlFor="birthdate">Date of Birth YYYY/MM/DD</label>
-                                <Field name="birthdate" type="text" className={'form-control' + (errors.birthdate && touched.birthdate ? ' is-invalid' : '')} />
-                                <ErrorMessage name="birthdate" component="div" className="invalid-feedback" />
-                            </div>
-                            <div className="form-group col">
-                                <label>Sex</label>
-                                <Field name="sex" as="select" className={'form-control'}>
-                                    <option value="Male">Male</option>
-                                    <option value="Female">Female</option>
-                                    <option value="Other">Genderqueer/Non-Binary</option>
-                                    <option value="">Prefer not to disclose</option>
-                                </Field>
-                            </div>
-                        </div>
-                        <div className="form-row">
-                            <div className="form-group col">
-                                <label htmlFor="city">City</label>
-                                <Field name="city" type="text" className={'form-control' + (errors.city && touched.city ? ' is-invalid' : '')} />
-                                <ErrorMessage name="city" component="div" className="invalid-feedback" />
-                            </div>
-                            <div className="form-group col">
-                                <label htmlFor="state">State</label>
-                                <Field name="state" type="text" className={'form-control' + (errors.state && touched.state ? ' is-invalid' : '')} />
-                                <ErrorMessage name="state" component="div" className="invalid-feedback" />
-                            </div>
-                            <div className="form-group col">
-                                <label htmlFor="zip">Zip</label>
-                                <Field name="zip" type="text" className={'form-control' + (errors.zip && touched.zip ? ' is-invalid' : '')} />
-                                <ErrorMessage name="zip" component="div" className="invalid-feedback" />
-                            </div>
-                            <div className="form-group col">
-                                <label htmlFor="country">Country</label>
-                                <Field name="country" type="text" className={'form-control' + (errors.country && touched.country ? ' is-invalid' : '')} />
-                                <ErrorMessage name="country" component="div" className="invalid-feedback" />
-                            </div>
-                        </div>
-                        <div className="form-row">
-                            <div className="form-group col">
-                                <label htmlFor="phone">Phone Number</label>
-                                <Field name="phone" type="text" className={'form-control'} />
-                            </div>
-                            <div className="form-group col">
-                                <label htmlFor="email">Email Adress</label>
-                                <Field name="email" type="email" className={'form-control' + (errors.email && touched.email ? ' is-invalid' : '')} />
-                                <ErrorMessage name="email" component="div" className="invalid-feedback" />
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="password">Password</label>
-                            <Field name="password" type="password" className={'form-control' + (errors.password && touched.password ? ' is-invalid' : '')} />
-                            <ErrorMessage name="password" component="div" className="invalid-feedback" />
-                        </div>
-                        <h4>Security Questions</h4>
-                        <div className="form-group">
-                            <label htmlFor="securityQuestion1">{securityQuestions[0]}</label>
-                            <Field name="securityQuestion1" type="text" className={'form-control' + (errors.securityQuestion1 && touched.securityQuestion1 ? ' is-invalid' : '')} />
-                            <ErrorMessage name="securityQuestion1" component="div" className="invalid-feedback" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="securityQuestion2">{securityQuestions[1]}</label>
-                            <Field name="securityQuestion2" type="text" className={'form-control' + (errors.securityQuestion2 && touched.securityQuestion2 ? ' is-invalid' : '')} />
-                            <ErrorMessage name="securityQuestion2" component="div" className="invalid-feedback" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="securityQuestion3">{securityQuestions[2]}</label>
-                            <Field name="securityQuestion3" type="text" className={'form-control' + (errors.securityQuestion3 && touched.securityQuestion3 ? ' is-invalid' : '')} />
-                            <ErrorMessage name="securityQuestion3" component="div" className="invalid-feedback" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="captcha">What's 47+31?</label>
-                            <Field name="captcha" type="text" className={'form-control' + (errors.captcha && touched.captcha ? ' is-invalid' : '')} />
-                            <ErrorMessage name="captcha" component="div" className="invalid-feedback" />
-                        </div>
-                        <div className="form-group">
-                            <button type="submit" className="btn btn-primary mr-2">Register</button>
-                            <button type="reset" className="btn btn-secondary">Reset</button>
-                        </div>
-                    </Form>
-                )}
-            </Formik>
->>>>>>> ec1a2cd1b7cc2393246469327a620247c6d68b7b:src/containers/Register.jsx
         </>
     )
 }
