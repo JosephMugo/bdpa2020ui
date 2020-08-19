9d43733b19a85cc9d0c8c7c3b88b9bcecc92b84a
JosephMugo <joemu18@gmail.com>
2020-08-18 19:59:00 -0500

    admin can now create a customer account

--- src/containers/AdminCreateUser.jsx
+++ src/containers/AdminCreateUser.jsx
@@ -1,50 +1,59 @@
-import React from "react"
-import * as Yup from 'yup'
+import React, { useState } from "react"
+// import { Form, Button, Col } from "react-bootstrap"
 import { Formik, Field, Form, ErrorMessage } from 'formik'
-import { createUser } from "../services/userService";
-
+import { object, string, date } from 'yup'
+import { parse, isDate } from "date-fns"
+import { createUser } from "../services/userService"
+const securityQuestions = [
+    "What was the house number and street name you lived in as a child?",
+    "What primary school did you attend?",
+    "What is your favorite food?"
+]
 const AdminCreateUser = () => {
+    const [registerResponse, setRegisterResponse] = useState(2)
+    const handleSubmit = async (fields) => {
+        const { captcha, ...info } = fields
+        info.birthdate = new Date(info.birthdate)
+        setRegisterResponse(3)
+        const response = await createUser(info)
+        setRegisterResponse(0 + response)
+    }
+    const required = string().required('Required')
     return (
         <>
-            <div className='row'>
-                <div className='col-sm-3'></div>
-                <div className='col-sm-6'>
+            <div align='center'>
+                <div className='col-sm-8'>
                     <hr />
-                    <h2 align='center'>Admin Create Customer Account</h2>
+                    <h2>Admin Customer Account Creation</h2>
+                    <hr />
+                    <p>Create a customer account</p>
                     <hr />
                     <Formik
                         initialValues={{
-                             firstName: "", lastName: "", birthdate: "", sex: "", city: "", state: "", zip: "", country: "",
-                            email: "", phone: "", password: "", captcha: ""
+                            title: "", firstName: "", middleName: "", lastName: "", suffix: "",
+                            birthdate: "", sex: "", city: "", state: "", zip: "", country: "",
+                            email: "", phone: "", username: "", password: "",
+                            securityQuestion1: "", securityQuestion2: "", securityQuestion3: "", captcha: ""
                         }}
-                        validationSchema={Yup.object().shape({
-                            firstName: Yup.string().required('Required'),
-                            lastName: Yup.string().required('Required'),
-                            birthdate: Yup.string().required('Required'),
-                            city: Yup.string().required('Required'),
-                            state: Yup.string().required('Required'),
-                            zip: Yup.string().required('Required'),
-                            country: Yup.string().required('Required'),
-                            email: Yup.string()
-                                .email('Email is invalid')
-                                .required('Required'),
-                            password: Yup.string()
+                        validationSchema={object().shape({
+                            username: required.matches(/^[a-zA-Z0-9]+$/, "Cannot contain special characters or spaces"),
+                            firstName: required, lastName: required,
+                            birthdate: date().required("Required").max(new Date(), "Invalid Date of Birth"),
+                            sex: required, city: required, state: required, zip: required, country: required,
+                            email: required.email('Email is invalid'), phone: string().matches(/^[0-9]+$/, "Can only contain numbers"),
+                            password: required
                                 .min(10, 'Must be at least 10 characters')
                                 .max(30, "Must be less than 30 characters")
-                                .required('Required')
                                 .matches(
                                     /^[a-zA-Z0-9!@#$%^&*?_~]+$/,
                                     "Cannot contain special characters or spaces"
                                 ),
-                        
-                            captcha: Yup.string().matches("78", 'Invalid Captcha').required('Required')
+                            securityQuestion1: required, securityQuestion2: required, securityQuestion3: required,
+                            captcha: required.matches("78", 'Invalid Captcha')
                         })}
-                        onSubmit={fields => {
-                            const { captcha, ...info } = fields
-                            createUser(info)
-                        }}
+                        onSubmit={handleSubmit}
                     >
-                        {({ errors, status, touched }) => (
+                        {({ errors, touched }) => (
                             <Form>
                                 <div className="form-row">
                                     <div className="form-group col-2">
@@ -56,13 +65,19 @@ const AdminCreateUser = () => {
                                         <Field name="firstName" type="text" className={'form-control' + (errors.firstName && touched.firstName ? ' is-invalid' : '')} />
                                         <ErrorMessage name="firstName" component="div" className="invalid-feedback" />
                                     </div>
-
+                                    <div className="form-group col-3">
+                                        <label htmlFor="middleName">Middle Name</label>
+                                        <Field name="middleName" type="text" className="form-control" />
+                                    </div>
                                     <div className="form-group col-3">
                                         <label htmlFor="lastName">Last Name</label>
                                         <Field name="lastName" type="text" className={'form-control' + (errors.lastName && touched.lastName ? ' is-invalid' : '')} />
                                         <ErrorMessage name="lastName" component="div" className="invalid-feedback" />
                                     </div>
-
+                                    <div className="form-group col-1">
+                                        <label htmlFor="suffix">Suffix</label>
+                                        <Field name="suffix" type="text" className="form-control" />
+                                    </div>
                                 </div>
                                 <div className="form-row">
                                     <div className="form-group col">
@@ -72,10 +87,13 @@ const AdminCreateUser = () => {
                                     </div>
                                     <div className="form-group col">
                                         <label>Sex</label>
-                                        <Field name="sex" as="select" className={'form-control'}>
-                                            <option value="Male">Male</option>
-                                            <option value="Female">Female</option>
+                                        <Field name="sex" as="select" className={'form-control' + (errors.sex && touched.sex ? ' is-invalid' : '')}>
+                                            <option value=""></option>
+                                            <option value="male">Male</option>
+                                            <option value="female">Female</option>
+                                            <option value="other">Genderqueer/Non-Binary</option>
                                         </Field>
+                                        <ErrorMessage name="sex" component="div" className="invalid-feedback" />
                                     </div>
                                 </div>
                                 <div className="form-row">
@@ -103,7 +121,8 @@ const AdminCreateUser = () => {
                                 <div className="form-row">
                                     <div className="form-group col">
                                         <label htmlFor="phone">Phone Number</label>
-                                        <Field name="phone" type="text" className={'form-control'} />
+                                        <Field name="phone" type="text" className={'form-control' + (errors.phone ? ' is-invalid' : '')} />
+                                        <ErrorMessage name="phone" component="div" className="invalid-feedback" />
                                     </div>
                                     <div className="form-group col">
                                         <label htmlFor="email">Email Adress</label>
@@ -112,24 +131,47 @@ const AdminCreateUser = () => {
                                     </div>
                                 </div>
                                 <div className="form-group">
+                                    <label htmlFor="username">Username</label>
+                                    <Field name="username" type="text" className={'form-control' + (errors.username && touched.username ? ' is-invalid' : '')} />
+                                    <ErrorMessage name="username" component="div" className="invalid-feedback" />
+                                </div>
+                                <div className="form-group">
                                     <label htmlFor="password">Password</label>
                                     <Field name="password" type="password" className={'form-control' + (errors.password && touched.password ? ' is-invalid' : '')} />
                                     <ErrorMessage name="password" component="div" className="invalid-feedback" />
                                 </div>
-
+                                <h4>Security Questions</h4>
+                                <div className="form-group">
+                                    <label htmlFor="securityQuestion1">{securityQuestions[0]}</label>
+                                    <Field name="securityQuestion1" type="text" className={'form-control' + (errors.securityQuestion1 && touched.securityQuestion1 ? ' is-invalid' : '')} />
+                                    <ErrorMessage name="securityQuestion1" component="div" className="invalid-feedback" />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="securityQuestion2">{securityQuestions[1]}</label>
+                                    <Field name="securityQuestion2" type="text" className={'form-control' + (errors.securityQuestion2 && touched.securityQuestion2 ? ' is-invalid' : '')} />
+                                    <ErrorMessage name="securityQuestion2" component="div" className="invalid-feedback" />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="securityQuestion3">{securityQuestions[2]}</label>
+                                    <Field name="securityQuestion3" type="text" className={'form-control' + (errors.securityQuestion3 && touched.securityQuestion3 ? ' is-invalid' : '')} />
+                                    <ErrorMessage name="securityQuestion3" component="div" className="invalid-feedback" />
+                                </div>
                                 <div className="form-group">
                                     <label htmlFor="captcha">What's 47+31?</label>
                                     <Field name="captcha" type="text" className={'form-control' + (errors.captcha && touched.captcha ? ' is-invalid' : '')} />
                                     <ErrorMessage name="captcha" component="div" className="invalid-feedback" />
                                 </div>
                                 <div className="form-group">
-                                    <button type="submit" className="btn btn-primary mr-2">AdminCreateUser</button>
+                                    <button type="submit" className="btn btn-primary mr-2">Register</button>
                                     <button type="reset" className="btn btn-secondary">Reset</button>
                                 </div>
+                                <h5>{["Username Already Taken", "User Registered!", "", "Loading..."][registerResponse]}</h5>
                             </Form>
                         )}
                     </Formik>
+
                 </div>
+                <div className='col-sm-3' />
             </div>
         </>
     )
