43e06ee450f686e3111b5e94b114f6f5c1802cfd
JosephMugo <joemu18@gmail.com>
2020-08-19 20:24:51 -0500

    display users on the admin dashboard (still have to implement deleting users)

--- src/containers/DashboardAdmin.jsx
+++ src/containers/DashboardAdmin.jsx
@@ -1,19 +1,44 @@
 import React, { useState, useEffect } from "react"
-import { requestAllUsers } from "../services/userService";
+import { requestAllUsers } from "../services/userService"
+import Table from 'react-bootstrap/Table'
+import { Button } from "react-bootstrap"
+import '../styles/DashboardAdmin.css'
+
 const DashboardAdmin = () => {
-    const [gotUsers, setGotUsers] = useState(false);
+    const [gotUsers, setGotUsers] = useState(false)
+    const [users, setUsers] = useState([])
 
     const getUserInfo = async () => {
         setGotUsers(true)
         const requestedUsers  = await requestAllUsers()
-        console.log("users", requestedUsers)
+        setUsers(requestedUsers)
     }
     useEffect(() => {
         if (!gotUsers) { getUserInfo() }
     })
     return (
     <div>
-       These are your tickets
+        <h3>Users</h3>
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Username</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                {users.map((user) => (
+                    <tr key={user.username}>
+                        <td>{user.firstName}</td>
+                        <td>{user.lastName}</td>
+                        <td>{user.username}</td>
+                        <td><Button className="deleteButton" variant="danger">Delete</Button></td>
+                    </tr>
+                ))}
+            </tbody>
+        </Table>
     </div>
     )
 }
